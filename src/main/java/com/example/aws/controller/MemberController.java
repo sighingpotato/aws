b91ece93c73f7d7c4f2a5d6dcbfe1791a2fc0861
sighingpotato <jot0202@naver.com>
2026-05-19 16:46:40 +0900

    refactor : MemberController 리팩토링

--- src/main/java/com/example/aws/controller/MemberController.java
+++ src/main/java/com/example/aws/controller/MemberController.java
@@ -5,6 +5,9 @@ import com.example.aws.entity.Member;
 import com.example.aws.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/members")
@@ -21,5 +24,19 @@ public class MemberController {
     @GetMapping("/{id}")
     public Member getMember(@PathVariable Long id) {
         return memberService.getMember(id);
+    }
+
+    // 프로필 사진 S3에 업로드 (POST)
+    @PostMapping("/{id}/profile-image")
+    public String uploadProfileImage(
+            @PathVariable Long id,
+            @RequestParam("file") MultipartFile File) throws IOException {
+        return memberService.uploadProfileImage(id, File);
+    }
+
+    // 7일짜리 임시 입장권 발급 (GET)
+    @GetMapping("/{id}/profile-image")
+    public String getProfileImage(@PathVariable Long id) {
+        return memberService.getProfileImageUrl(id);
     }
 }
